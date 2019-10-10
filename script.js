d2622ea49e34d6606927bdb648bc0172352188cb
RachaelChar <charewicz.r@gmail.com>
2019-10-10 11:41:00 -0400

    add onclick feature to generate button

--- script.js
+++ script.js
@@ -1,73 +1,71 @@
-// Initial prompt for user's desired number of characters
-var userInput = prompt("Please enter a number between 8 and 128 to determine the number of characters in your password.");
-//  The user's input is a string, so we need parseInt() to turn it into a number
-var numberOfChar = parseInt(userInput);
 
 
-// Checks to see that the user has enterd a number between 8 and 128
-if (numberOfChar < 8 || numberOfChar > 128) {
-    alert("Please enter a number between 8 and 128 to continue.");
-    prompt("Please enter a number between 8 and 128 to determine the number of characters in your password.");
-    parseInt(userInput);
-}
-
-// Asks for the user's preferences as to what they would like in their password
-var confirmLowerCase = confirm("Would you like your password to include lowercase letters?");
-var confirmUpperCase = confirm("Would you like your password to include uppercase letters?");
-var confirmSpecialChar = confirm("Would you like your password to include special characters?");
-var confirmNumbers = confirm("Would you like your password to include numbers?");
-
-if (confirmLowerCase == false && confirmUpperCase == false && confirmSpecialChar == false && confirmNumbers == false){
-    alert("Please select at least one type of character for your password.");
-    confirmLowerCase = confirm("Would you like your password to include lowercase letters?");
-    confirmUpperCase = confirm("Would you like your password to include uppercase letters?");
-    confirmSpecialChar = confirm("Would you like your password to include special characters?");
-    confirmNumbers = confirm("Would you like your password to include numbers?");
-}
-
-
-// Define variables for each type of character the user can choose to include in their password
-var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
-var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var numbers = ["0123456789"];
-var specialChar= ["!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
-
-
-// This line connects the clicking of the generate button to the calling of the function that generates the password.
+// This connects the clicking of the generate button to the calling of the function that generates the password.
 // Source: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_addeventlistener
-document.getElementById("generate").addEventListener("click", function(){
-    for (var i = 0; i < numberOfChar; i++) {
+function generatePassword() {
 
+       // Initial prompt for user's desired number of characters
+       var userInput = prompt("Please enter a number between 8 and 128 to determine the number of characters in your password.");
+       //  The user's input is a string, so we need parseInt() to turn it into a number
+       var numberOfChar = parseInt(userInput);
+   
+   
+       // Checks to see that the user has enterd a number between 8 and 128
+       if (numberOfChar < 8 || numberOfChar > 128) {
+           alert("Please enter a number between 8 and 128 to continue.");
+           prompt("Please enter a number between 8 and 128 to determine the number of characters in your password.");
+           parseInt(userInput);
+       }
+   
+       // Asks for the user's preferences as to what they would like in their password
+       var confirmLowerCase = confirm("Would you like your password to include lowercase letters?");
+       var confirmUpperCase = confirm("Would you like your password to include uppercase letters?");
+       var confirmSpecialChar = confirm("Would you like your password to include special characters?");
+       var confirmNumbers = confirm("Would you like your password to include numbers?");
+   
+       if (confirmLowerCase == false && confirmUpperCase == false && confirmSpecialChar == false && confirmNumbers == false) {
+           alert("Please select at least one type of character for your password.");
+           confirmLowerCase = confirm("Would you like your password to include lowercase letters?");
+           confirmUpperCase = confirm("Would you like your password to include uppercase letters?");
+           confirmSpecialChar = confirm("Would you like your password to include special characters?");
+           confirmNumbers = confirm("Would you like your password to include numbers?");
+       }
+    };
+
+// Define variables for each type of character the user can choose to include in their password.
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialChar = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+
+// function password(numberOfCha r, ) {
+//     var randomPassword = "";
+// }
+
+for (var i = 0; i < numberOfChar; i++) {
+    if (confirmLowerCase == true) {
+        password = password + (lowerCase.charAt(Math.floor(Math.random() * lowerCase.length)));
     }
 
-});       
-   
+    if (confirmUpperCase == true) {
+        password = password + (upperCase.charAt(Math.floor(Math.random() * upperCase.length)));
+    }
 
-    //     var lowerCase = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    if (confirmSpecialChar == true) {
+        password = password + (specialChar.charAt(Math.floor(Math.random() * specialChar.length)));
+    }
 
-    //     var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    if (confirmNumbers == true) {
+        password = password + (numbers.charAt(Math.floor(Math.random() * numbers.length)));
+    }
 
-    //     // In order to include certain special characters in HTML, you have to put a \ before them
-    //     var specialChar = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-
-    //     var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    // var numCharacters = prompt("Please choose a number between 8 and 128 to determine how many characters your password will contain.");
-
-
-    //     // Function to generate the password
-    //     function password(length) {
-    //         for (var i = length; i < 129; i++){
-    //             // selects a random character 
-    //             charAt(Math.floor(Math.random() * lowerCase.length));  
-    //             charAt(Math.floor(Math.random() * upperCase.length));
-    //             charAt(Math.floor(Math.random() * specialChar.length));
-    //             charAt(Math.floor(Math.random() * num.length));
-    //         }
-
-    //         alert(password())
-    //     }
+}
 
 
 
 
+
+    // This line changes the placeholder text on the page with the generated password.
+//     document.getElementById("passwordDisplay").placeholder = Password;
+// }\
